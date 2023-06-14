40863104e7eae3fe6470cae5521719b83a174c81
Math Scicl <scicluna.mathieu@hotmail.fr>
2023-06-14 15:02:37 +0200

    ajout bullet point + rendre actif

--- Print-it-JS-main/assets/script.js
+++ Print-it-JS-main/assets/script.js
@@ -23,7 +23,11 @@ console.log(numero);
 
 const Suivant = document.querySelector(".arrow_left");
 const Precedent = document.querySelector(".arrow_right");
-const dots = document.querySelector(".dot")
+const dots = document.querySelector(".dots");
+slides.forEach(function (dot) {
+  /** Création boucle dans le tableau 'slides' **/ dots.innerHTML +=
+    '<div class="dot"></div>'; /** On a insérer la <div class = "dot"> dans la balise "dots" dans notre DOM **/
+});
 
 console.log(Precedent);
 console.log(Suivant);
@@ -45,17 +49,36 @@ function ChangeSlide(sens) {
   if (numero < 0) numero = 3;
   document.getElementById("slide").src =
     "assets/images/slideshow/" + slides[numero].image;
+
+  const dotSelected =
+    document.querySelectorAll(
+      ".dot"
+    ); /** On déclare la const 'dot_selected' en selectionnant la class '.dot' **/
+
+  dot_selected.forEach((dot_selected) => {
+    /** Création boucle forEach **/
+
+    dot.classList.remove(
+      "dot_selected"
+    ); /** suppression des class 'dot' remplacer par 'dot_selected **/
+  });
+  dotSelected[numero].classList.add(
+    "dot_selected"
+  ); /** Ajout de la classe 'dot_selected au DOM **/
+
+  console.log(dotSelected);
 }
 
 function dotActive() {
-  dots.forEach(dot => {
-    dot.classList.remove('dot_selected');
+  dot_selected.forEach((dot) => {
+    dot.classList.remove("dot_selected");
   });
-  dots[numero].classList.add('dot_selected');
+  dots[numero].classList.add("dot_selected");
   console.log(dots);
 }
 
 function textBanner() {
   bannerImg.src = "assets/images/slideshow/" + slides[slidePosition].image;
-  document.querySelector("#banner > p").innerHTML = slides[slidePosition].tagLine
+  document.querySelector("#banner > p").innerHTML =
+    slides[slidePosition].tagLine;
 }
