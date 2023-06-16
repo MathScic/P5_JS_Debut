192a66453f1ad2c41542fdcd143e5923811361a1
Math Scicl <scicluna.mathieu@hotmail.fr>
2023-06-16 10:54:07 +0200

    modif function changeSlide + ajout bullet point

--- Print-it-JS-main/assets/script.js
+++ Print-it-JS-main/assets/script.js
@@ -24,9 +24,16 @@ console.log(numero);
 const Suivant = document.querySelector(".arrow_left");
 const Precedent = document.querySelector(".arrow_right");
 const dots = document.querySelector(".dots");
+const dotSelected =
+  document.querySelectorAll(
+    ".dot"
+  ); /** On déclare la const 'dot_selected' en selectionnant la class '.dot' **/
+
 slides.forEach(function (dot) {
-  /** Création boucle dans le tableau 'slides' **/ dots.innerHTML +=
-    '<div class="dot"></div>'; /** On a insérer la <div class = "dot"> dans la balise "dots" dans notre DOM **/
+  /** Création boucle dans le tableau 'slides' **/
+
+  dots.innerHTML += '<div class="dot"></div>';
+  /** On a insérer la <div class = "dot"> dans la balise "dots" dans notre DOM **/
 });
 
 console.log(Precedent);
@@ -44,18 +51,18 @@ Precedent.addEventListener("click", () => {
 });
 
 function ChangeSlide(sens) {
-  numero = numero + sens;
-  if (numero > 3) numero = 0;
-  if (numero < 0) numero = 3;
+numero += sens;
+	if(numero > slides.length-1) {
+			numero = 0;
+		} else if(numero == -1)  {
+			numero = slides.length-1;
+			
+		}
+
   document.getElementById("slide").src =
     "assets/images/slideshow/" + slides[numero].image;
 
-  const dotSelected =
-    document.querySelectorAll(
-      ".dot"
-    ); /** On déclare la const 'dot_selected' en selectionnant la class '.dot' **/
-
-  dotSelected.forEach((dot_selected) => {
+  dotSelected.forEach((dot) => {
     /** Création boucle forEach **/
 
     dotSelected.classList.remove(
@@ -67,18 +74,4 @@ function ChangeSlide(sens) {
   ); /** Ajout de la classe 'dot_selected au DOM **/
 
   console.log(dotSelected);
-}
-
-function dotActive() {
-  dot_selected.forEach((dot) => {
-    dot.classList.remove("dot_selected");
-  });
-  dots[numero].classList.add("dot_selected");
-  console.log(dots);
-}
-
-function textBanner() {
-  bannerImg.src = "assets/images/slideshow/" + slides[slidePosition].image;
-  document.querySelector("#banner > p").innerHTML =
-    slides[slidePosition].tagLine;
 }
